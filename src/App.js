243587a0ba8235923b7eb039d42a784d03b16c6a
Priyanshu Pawar <talk20priyanshu@gmail.com>
2023-12-09 23:53:57 +0530

    logicalAss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
-import ClassAssignment2 from "./Components/ClassAssignment2";
+import ClassAssignment3 from "./Components/ClassAssignment3";
+// import Mcqassign from "./Components/Mcqassign";
+// import ClassAssignment2 from "./Components/ClassAssignment2";
 // import AssignmentFives from "./Components/AssignmentFives";
 // import AssignmentSeven from "./Components/AssignmentSeven";
 // import ClassAssignment from "./Components/ClassAssignment";
@@ -12,7 +14,9 @@ function App() {
       {/* <AssignmentFives /> */}
       {/* <ClassAssignment /> */}
       {/* <AssignmentSeven /> */}
-      <ClassAssignment2 />
+      {/* <ClassAssignment2 /> */}
+      {/* <Mcqassign /> */}
+      <ClassAssignment3 />
     </>
   );
 }
---
+++ src/Components/Mcqassign.jsx
@@ -0,0 +1,291 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Mcqassign() {
+  return (
+    <>
+      <h1 style={{ textAlign: "center", color: "GrayText" }}>JavaScript MCQ</h1>
+      <div>
+        <div className="container-fluid mt-5 w-50 ">
+          <label>(a) Which type of JavaScript language is ________</label>
+          <div className="conatiner-fluid  ">
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Object-Oriented</label>
+            </div>
+
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Object-Based</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Assembly-language</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">High-level</label>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid mt-5 w-50 ">
+          <label>
+            (b) Which one of the following also known as Conditional Expression:
+          </label>
+          <div className="conatiner-fluid  ">
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Alternative to if-else</label>
+            </div>
+
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Switch statement</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">If-then-else statement</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">immediate if</label>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid mt-5 w-50 ">
+          <label>(c) In JavaScript, what is a block of statement?</label>
+          <div className="conatiner-fluid  ">
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Conditional block</label>
+            </div>
+
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">
+                block that combines a number of statements into a single
+                compound statement
+              </label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">
+                both conditional block and a single statement
+              </label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">
+                block that contains a single statement
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid mt-5 w-50 ">
+          <label>
+            (d) When interpreter encounters an empty statements, what it will
+            do:
+          </label>
+          <div className="conatiner-fluid  ">
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Shows a warning</label>
+            </div>
+
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">
+                Prompts to complete the statement
+              </label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Throws an error</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Ignores the statements</label>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid mt-5 w-50 ">
+          <label>(e) The "function" and " var" are known as:</label>
+          <div className="conatiner-fluid  ">
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Keywords</label>
+            </div>
+
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Data types</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Declaration statements</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Prototypes</label>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid mt-5 w-50 ">
+          <label>
+            (f) Which one of the following is the correct way for calling the
+            JavaScript code?
+          </label>
+          <div className="conatiner-fluid  ">
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Preprocessor</label>
+            </div>
+
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Triggering Event</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">RMI</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Function/Method</label>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid mt-5 w-50 ">
+          <label>
+            (g) Which of the following type of a variable is volatile?
+          </label>
+          <div className="conatiner-fluid  ">
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Mutable variable</label>
+            </div>
+
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Dynamic variable</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Volatile variable</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Immutable variable</label>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid mt-5 w-50 ">
+          <label>
+            (h) When there is an indefinite or an infinite value during an
+            arithmetic computation in a program, then JavaScript prints______.
+          </label>
+          <div className="conatiner-fluid  ">
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">
+                Prints an exception error
+              </label>
+            </div>
+
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">
+                Prints an overflow error
+              </label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Displays "Infinity"</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">
+                Prints the value as such
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid mt-5 w-50 ">
+          <label>
+            (i) In the JavaScript, which one of the following is not considered
+            as an error:
+          </label>
+          <div className="conatiner-fluid  ">
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Syntax error</label>
+            </div>
+
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Missing of semicolons</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Division by zero</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="checkbox" value="" />
+              <label className="form-check-label">Missing of Bracket</label>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid mt-5 w-50 ">
+          <label>
+            (j) Which of the following number object function returns the value
+            of the number?
+          </label>
+          <div className="conatiner-fluid  ">
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="radio" value="" name="1" />
+              <label className="form-check-label">toString()</label>
+            </div>
+
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="radio" name="1" value="" />
+              <label className="form-check-label">valueOf()</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="radio" name="1" value="" />
+              <label className="form-check-label">toLocaleString()</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="radio" name="1" value="" />
+              <label className="form-check-label">toPrecision()</label>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid mt-5 w-50 ">
+          <label>
+            (k) Which of the following number object function returns the value
+            of the number?
+          </label>
+          <div className="conatiner-fluid  ">
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="radio" value="" name="1" />
+              <label className="form-check-label">toString()</label>
+            </div>
+
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="radio" name="1" value="" />
+              <label className="form-check-label">Missing of semicolons</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="radio" name="1" value="" />
+              <label className="form-check-label">Division by zero</label>
+            </div>
+            <div className="d-flex gap-3">
+              <input class="form-check-input" type="radio" name="1" value="" />
+              <label className="form-check-label">Missing of Bracket</label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
