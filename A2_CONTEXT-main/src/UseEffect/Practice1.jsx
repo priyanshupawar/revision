77c346256d1d0182f2dd5847d6d94d09f870449c
Priyanshu Pawar <talk20priyanshu@gmail.com>
2023-12-15 18:19:25 +0530

    useEffectassignmentClass

--- A2_CONTEXT-main/src/UseEffect/Practice1.jsx
+++ A2_CONTEXT-main/src/UseEffect/Practice1.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function Practice1() {
+  const [color, setColor] = useState("red");
+  useEffect(() => {
+    let c1 = Math.trunc(Math.random() * 250);
+    let c2 = Math.trunc(Math.random() * 250);
+    let c3 = Math.trunc(Math.random() * 250);
+    // let clr = (rgb)
+    // console.log(color:{})
+  }, []);
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div style={{ margin: "auto" }}>
+        <button>Start</button>
+        <button>End</button>
+      </div>
+      <div
+        style={{
+          width: "90%",
+          marginTop: "60px",
+          margin: "auto",
+          display: "grid",
+          gridTemplateColumns: "repeat(3,1fr)",
+          justifyContent: "space-evenly",
+          gap: "35px 20px",
+          padding: "20px",
+        }}
+      >
+        {arr.map((ele, i) => {
+          return (
+            <div
+              style={{
+                color: color,
+                // border: "1px solid red",
+                height: "350px",
+                width: "350px",
+                borderRadius: "15px",
+                boxShadow:
+                  " rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
+              }}
+            ></div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
-export default Practice1
+export default Practice1;
---
+++ assignmentappnew/src/App.js
@@ -0,0 +1,17 @@
+ import React from 'react'
+import Filter from './component/Filter'
+import Card from './component/Card'
+ 
+ function App() {
+   return (
+      <>
+      <div className='container-fluid'>
+      <Filter/>
+      <Card/>
+      </div>
+     
+      </>
+   )
+ }
+ 
+ export default App
