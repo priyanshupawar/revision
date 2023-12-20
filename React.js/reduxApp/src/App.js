f5eca2bd541ede3cd632c050e64f65f9070f4be9
Priyanshu Pawar <talk20priyanshu@gmail.com>
2023-12-20 18:15:20 +0530

    ReduxData

---
+++ React.js/reduxApp/src/App.js
@@ -0,0 +1,21 @@
+ import React from 'react'
+import Counter from './Counter'
+import Counter1 from './Counter1'
+import Print2 from './Print2'
+import Card from './Card'
+ 
+ function App() {
+   return (
+     <div>App
+      {/* <Counter/> */}
+      <Counter1/>
+      <hr/>
+      <Print2/>
+      <hr/>
+      <br/>
+      <Card/>
+     </div>
+   )
+ }
+ 
+ export default App
