5f358158f670346328066264ddac3c936973601c
Priyanshu Pawar <talk20priyanshu@gmail.com>
2023-12-21 14:45:14 +0530

    Redux

--- React.js/reduxApp/src/Card.jsx
+++ React.js/reduxApp/src/Card.jsx
@@ -1,20 +1,51 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+// import React from 'react'
+// import { useSelector } from 'react-redux'
+
+// function Card() {
+//   let x=  useSelector((item)=>{
+//         return item.saveReducer
+//     })
+//   return (
+//      <>
+//      {x.data.length===0 && <h1>no data </h1>}
+//      {x.data.length>0&& <>
+//         {x.data.map((item)=>{
+//             return  <>   <h1>{item}</h1> <br/>  </> 
+//         })}
+//      </>}
+//      </>
+//   )
+// }
+
+// export default Card.
+
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Card() {
-  let x=  useSelector((item)=>{
-        return item.saveReducer
-    })
+  let x = useSelector((item) => {
+    return item.saveReducer;
+  });
+
   return (
-     <>
-     {x.data.length===0 && <h1>no data </h1>}
-     {x.data.length>0&& <>
-        {x.data.map((item)=>{
-            return  <>   <h1>{item}</h1> <br/>  </> 
-        })}
-     </>}
-     </>
-  )
+    <>
+   
+      {x.data.length === 0 && <h1>no data </h1>}
+      {x.data.length > 0 && (
+        <>
+          {x.data.map((item, index) => {
+            return (
+              <div key={index} className="card">
+                <div className="card-body">
+                  <h1 className="card-title">{item}</h1>
+                </div>
+              </div>
+            );
+          })}
+        </>
+      )}
+    </>
+  );
 }
 
-export default Card
+export default Card;
--- React.js/reduxApp/src/Print2.jsx
+++ React.js/reduxApp/src/Print2.jsx
@@ -1,24 +1,65 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { saveHandler } from './redux/action/actionCreater'
+// import React, { useState } from 'react'
+// import { useDispatch } from 'react-redux'
+// import { saveHandler } from './redux/action/actionCreater'
+
+// function Print2() {
+//    let [input,setInput]= useState('')
+//       let dispatch= useDispatch()
+//    function inputHandler(e)
+//    {
+//     setInput(e.target.value)
+//    }
+//   return (
+//     <div>
+//         <input type='text' placeholder='enter your msg for print'
+//         onChange={inputHandler} value={input}/>
+//         <button onClick={()=>{
+//                dispatch(saveHandler(input))
+//                setInput('')
+//         }}>ADD</button>
+//     </div>
+//   )
+// }
+
+// export default Print2
+
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { saveHandler } from "./redux/action/actionCreater";
 
 function Print2() {
-   let [input,setInput]= useState('')
-      let dispatch= useDispatch()
-   function inputHandler(e)
-   {
-    setInput(e.target.value)
-   }
+  let [input, setInput] = useState("");
+  let dispatch = useDispatch();
+
+  function inputHandler(e) {
+    setInput(e.target.value);
+  }
+
   return (
-    <div>
-        <input type='text' placeholder='enter your msg for print'
-        onChange={inputHandler} value={input}/>
-        <button onClick={()=>{
-               dispatch(saveHandler(input))
-               setInput('')
-        }}>ADD</button>
+    <div className="container mt-3">
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter your message for print"
+          onChange={inputHandler}
+          value={input}
+        />
+        <div className="input-group-append">
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={() => {
+              dispatch(saveHandler(input));
+              setInput("");
+            }}
+          >
+            ADD
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Print2
+export default Print2;
