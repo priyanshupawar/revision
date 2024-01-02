c27ca1d859e9d4689aae353eb622c860bbfcc7ce
Priyanshu Pawar <talk20priyanshu@gmail.com>
2024-01-02 23:41:43 +0530

    StateManagmentApp

--- DailyWork/december/sprint-3/day-7/we/statemanagemnetapp/src/App.js
+++ DailyWork/december/sprint-3/day-7/we/statemanagemnetapp/src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
-import './App.css';
-import Counter from './Components/Counter';
+import "./App.css";
+import Counter from "./Components/Counter";
 
 function App() {
   return (
     <div className="App">
-    <Counter/>
+      <Counter />
     </div>
   );
 }
--- DailyWork/december/sprint-3/day-7/we/statemanagemnetapp/src/Components/Counter.jsx
+++ DailyWork/december/sprint-3/day-7/we/statemanagemnetapp/src/Components/Counter.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1>Counter</h1>
+      <h1>Counter-:{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        {" "}
+        Decrement
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
