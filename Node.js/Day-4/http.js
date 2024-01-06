fb0a71ede666a97e1538cd5a8294402e082e050c
Priyanshu Pawar <talk20priyanshu@gmail.com>
2024-01-06 16:01:41 +0530

    ServerCreated

--- Node.js/Day-4/http.js
+++ Node.js/Day-4/http.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((request, response) => {
   console.log(request.url);
@@ -7,6 +8,15 @@ const server = http.createServer((request, response) => {
   if (request.method == "GET") {
     if (request.url == "/") {
       response.write("Hello from backend server Namaskaram ");
+      response.end();
+    }
+    if (request.url == "/about") {
+      response.write("This is about page ******************** ");
+      response.end();
+    }
+    if (request.url == "/contact") {
+      let data = fs.readFileSync("./msg.txt", "utf-8");
+      response.write(data);
       response.end();
     }
   }
