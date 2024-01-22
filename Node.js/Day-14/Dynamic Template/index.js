0a4c2f2097302cc9744a1e2dcba36fee7c1d0e3c
Priyanshu Pawar <talk20priyanshu@gmail.com>
2024-01-22 16:45:09 +0530

    AddedCSS

--- Node.js/Day-14/Dynamic Template/index.js
+++ Node.js/Day-14/Dynamic Template/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { join } = require("path");
 
 const app = express();
 
@@ -6,6 +7,9 @@ const PORT = 7000;
 const hostName = "127.0.0.7";
 
 app.set("view engine", "ejs");
+
+app.use(express.static(join(__dirname, "public")));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.render("Home", { name: "Priyanshu", address: "Indore" });
