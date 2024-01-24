0b0d7e5a19630e13d218f594aee405c038668e55
Priyanshu Pawar <talk20priyanshu@gmail.com>
2024-01-24 16:28:22 +0530

    MongodbServer

--- Node.js/Day-13/index.js
+++ Node.js/Day-13/index.js
@@ -3,3 +3,15 @@ const app = express;
 
 const PORT = 7000;
 const hostName = "127.0.0.7";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/post",(req,res)=>{
+    console.log(req.body)
+    res.status(201).send("New POST request from data recive")
+})
+
+app.listen(PORT, hostName, () => {
+  console.log(`Server Started in http://${hostName}:${PORT}`);
+});
